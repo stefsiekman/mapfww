589238ea65d6b124d7321270e75f952f675137f8
Stef Siekman <s.siekman@gmail.com>
2020-04-21 12:17:48 +0200

    Change to example with waiting

--- app/main.js
+++ app/main.js
@@ -2,13 +2,15 @@ import {Window} from './window.js';
 import {Grid} from './grid.js';
 
 const grid = new Grid(5, 4);
+grid.addWall(0, 1);
 grid.addWall(1, 1);
 grid.addWall(1, 2);
 grid.addWall(3, 1);
+grid.addWall(4, 1);
 grid.addWall(3, 3);
 grid.addWall(4, 3);
-grid.addAgent([0, 0], [2, 3]);
-grid.addAgent([4, 0], [2, 2]);
+grid.addAgent([0, 0], [0, 2]);
+grid.addAgent([4, 0], [4, 2]);
 // const grid = new Grid(5, 3);
 // grid.addWall(0, 1);
 // grid.addWall(1, 1);
