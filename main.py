8e26b5fc595cebded009dbc88f614eaaa690570f
Stef Siekman <s.siekman@gmail.com>
2020-04-27 17:43:18 +0200

    Move solve code to module

--- main.py
+++ main.py
@@ -2,10 +2,8 @@ import requests
 import json
 from time import time
 
-from queue import PriorityQueue, Queue
-
 from grid import Grid
-from node import Node
+from solver import solve_od
 
 URL = "https://mapfw.nl/"
 headers = {
@@ -15,8 +13,8 @@ headers = {
 
 def load_problem(benchmark=1):
     data = {
-        "algorithm": "CBS",
-        "version": "0"
+        "algorithm": "A*+OD",
+        "version": "testing"
     }
     r = requests.post(f"{URL}api/benchmarks/{benchmark}/problems",
                       headers=headers, json=data)
@@ -48,60 +46,27 @@ def post_solution(attempt_id, problem_id, time, solution):
 
 if __name__ == "__main__":
     attempt_id, problem_id, problem = load_problem()
-    start_time = time()
 
     print(f"Creating grid of {problem['width']}x{problem['height']}...")
     grid = Grid(problem["width"], problem["height"])
 
     print("Adding walls...")
-    walls = 0
     for y in range(problem["height"]):
         for x in range(problem["width"]):
             if problem["grid"][y][x] == 1:
                 grid.add_wall(x, y)
-                walls += 1
-    print(f"    added {walls} walls")
 
     print("Adding agents...")
     for start, goal in zip(problem["starts"], problem["goals"]):
-        print(f"    from {start}  \tto {goal}")
         grid.add_agent(tuple(start), tuple(goal))
 
     print()
     print("Solving...")
 
-    open_nodes = PriorityQueue()
-    node_id = 0
-    open_nodes.put((0, node_id, grid.root_node()))
-    node_id += 1
-
-    final_node: Node = None
-
-    while not open_nodes.empty():
-        f, id, node = open_nodes.get()
-
-        if node.all_done():
-            final_node = node
-            break
-
-        for new_node in node.expand():
-            # print(f"Adding new node to queue: {new_node.f} {new_node}")
-            item = (new_node.f, node_id, new_node)
-            node_id += 1
-            open_nodes.put(item)
-
+    start_time = time()
+    solution = solve_od(grid)
     time_taken = round((time() - start_time) * 1000)
-    print(f"Done in {time_taken}ms, with {node_id} nodes")
 
-    positions = []
-    while final_node is not None:
-        if final_node.is_standard():
-            positions.append([list(p) for p in final_node.positions])
-        final_node = final_node.parent
-
-    positions.reverse()
-
-    solution = [[positions[t][i] for t in range(len(positions))]
-                for i in range(grid.agents)]
+    print(f"Done in {time_taken}ms")
 
     post_solution(attempt_id, problem_id, time_taken, solution)
--- solver.py
+++ solver.py
@@ -1,7 +1,38 @@
-class Solver:
+from queue import PriorityQueue
 
-    def __init__(self, grid):
-        self.grid = grid
+from node import Node
 
-    def solve(self):
-        pass
+
+def create_solution(grid, node):
+    positions = []
+    while node is not None:
+        if node.is_standard():
+            positions.append([list(p) for p in node.positions])
+        node = node.parent
+
+    positions.reverse()
+
+    return [[positions[t][i] for t in range(len(positions))]
+            for i in range(grid.agents)]
+
+
+def solve_od(grid):
+    open_nodes = PriorityQueue()
+    node_id = 0
+    open_nodes.put((0, node_id, grid.root_node()))
+    node_id += 1
+
+    while not open_nodes.empty():
+        f, id, node = open_nodes.get()
+
+        if node.all_done():
+            return create_solution(grid, node)
+
+        for new_node in node.expand():
+            item = (new_node.f, node_id, new_node)
+            node_id += 1
+            open_nodes.put(item)
+
+
+def solve_od_if(grid):
+    return solve_od(grid)
