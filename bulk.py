1fd717591fc8db4464a90e4134a1edbe83332df0
Stef Siekman <s.siekman@gmail.com>
2020-05-18 15:18:16 +0200

    Divide work over worker threads

--- bulk.py
+++ bulk.py
@@ -4,13 +4,40 @@ from multiple sources, and the running can be stopped at any time.
 """
 import multiprocessing
 import os
+import time
+from queue import Queue
+from threading import Thread
 
 from git import Repo
 
 
+def work(index, busy_queue: Queue, result_queue: Queue):
+    print(f"Starting worker #{index}")
+
+    while True:
+        agents, waypoints = busy_queue.get(block=True)
+        print(f"[Worker {index}] Running with {agents}, {waypoints}")
+        time.sleep(2)
+        print(f"[Worker {index}] done")
+        busy_queue.put((agents, waypoints))
+
+
 def run_bulk(name, agent_range, waypoint_range):
     thread_number = thread_count()
-    pass
+
+    busy_queue = Queue()
+    result_queue = Queue()
+
+    # Populate with all combinations within range
+    for agents in range(agent_range[0], agent_range[1] + 1):
+        for waypoints in range(waypoint_range[0], waypoint_range[1] + 1):
+            busy_queue.put((agents, waypoints))
+
+    workers = [Thread(target=work, args=(i, busy_queue, result_queue),
+                      name=f"Worker-{i}")
+               for i in range(thread_number)]
+
+    # Collect results...
 
 
 def thread_count():
