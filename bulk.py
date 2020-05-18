8e1951f085c684a84077786da7e9c22c8f44fbe0
Stef Siekman <s.siekman@gmail.com>
2020-05-18 15:05:33 +0200

    Ask for thread count

--- bulk.py
+++ bulk.py
@@ -2,13 +2,25 @@
 Class for running benchmarks in bulk on some server. Results can be obtained
 from multiple sources, and the running can be stopped at any time.
 """
+import multiprocessing
 import os
 
 from git import Repo
 
 
 def run_bulk(name, agent_range, waypoint_range):
+    thread_number = thread_count()
     pass
+
+
+def thread_count():
+    cores = multiprocessing.cpu_count()
+    print(f"This system has {cores} cores")
+    inp = input("Reserve cores [0]:")
+    reserves = int(inp) if inp is not "" else 0
+    assert 0 <= reserves < cores, "Valid reserve count"
+
+    return cores - reserves
 
 
 def validate_repo():
@@ -27,7 +39,11 @@ if __name__ == "__main__":
         print("Commit all the code changes first")
         exit()
 
-    min_agents = int(input("Min. # agents:    "))
-    max_agents = int(input("Max. # agents:    "))
-    min_waypoints = int(input("Min. # waypoints: "))
-    max_waypoints = int(input("Max. # waypoints: "))
+    min_agents = int(input("Min # agents: "))
+    max_agents = int(input("Max # agents: "))
+    min_waypoints = int(input("Min # waypoints: "))
+    max_waypoints = int(input("Max # waypoints: "))
+
+    run_bulk(head_hex,
+             (min_agents, max_agents),
+             (min_waypoints, max_waypoints))
